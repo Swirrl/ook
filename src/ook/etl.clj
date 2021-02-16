6142f8b37091d0411a93838e1a12ebb9a58eeb59
Robin Gower <robin.git@infonomics.ltd.uk>
2021-02-16 17:43:41 +0100

    Create extract-datasets, extract-codes and extract-components
    - introduce vcr-clj for recording clj-http request/responses
    - functions to extracting datasets, components and codes
    - test extract-datasets against cogs-staging with graph constraint

--- src/ook/etl.clj
+++ src/ook/etl.clj
@@ -1,10 +1,29 @@
 (ns ook.etl
   (:require
-   [drafter-client.client :as dc]
    [drafter-client.client.impl :as dci]
-   [grafter-2.rdf4j.repository :as gr-repo]))
+   [clojure.java.io :as io]))
 
-(defn extract-datasets [{:keys [drafter-client/client] :as system}]
-  (let [repo (dc/->repo client nil dc/live)]
-    (with-open [conn (gr-repo/->connection repo)]
-      (doall (gr-repo/query conn "SELECT * WHERE { ?s ?p ?o } LIMIT 10")))))
+(defn construct
+  "Gets a construct query from the live draftset"
+  ([{:keys [drafter-client/client] :as system} query & opts]
+   (let [args (concat [client query] opts)
+         response (apply dci/get-query-live args)]
+     (:body response))))
+
+(defn constructor [query]
+  "Creates a function for calling construct queries with optional graph restrictions"
+  (fn
+    ([system] (construct system query))
+    ([system graphs] (construct system query :default-graph-uri graphs))))
+
+(def extract-datasets
+  (constructor (slurp (io/resource "etl/dataset-construct.sparql"))))
+
+(def extract-codes
+  (constructor (slurp (io/resource "etl/code-construct.sparql"))))
+
+(def extract-components
+  (constructor (slurp (io/resource "etl/component-construct.sparql"))))
+
+
+
--- test/ook/etl_test.clj
+++ test/ook/etl_test.clj
@@ -2,11 +2,16 @@
   (:require [clojure.test :refer :all]
             [integrant.core :as ig]
             [ook.concerns.integrant :as i]
+            [clj-http.client :as client]
+            [vcr-clj.clj-http :refer [with-cassette]]
             [ook.etl :as sut]))
 
 (deftest extract-test
   (testing "Extract data from a drafter endpoint"
-    (let [system (i/exec-config {:profiles ["drafter-client.edn", "cogs-staging.edn"]})
-          datasets (sut/extract-datasets system)]
-      (is (= 10 (count datasets)))
-      (ig/halt! system))))
+    (with-cassette :extract-datasets
+      (let [system (i/exec-config {:profiles ["drafter-client.edn", "cogs-staging.edn"]})
+            graphs ["http://gss-data.org.uk/graph/gss_data/trade/ons-exports-of-services-by-country-by-modes-of-supply-metadata"
+                    "http://gss-data.org.uk/graph/gss_data/trade/ons-exports-of-services-by-country-by-modes-of-supply"]
+            datasets (sut/extract-datasets system graphs)]
+        (is (= 13 (count datasets)))
+        (ig/halt! system)))))
