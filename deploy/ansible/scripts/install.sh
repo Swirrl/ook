4dcaf81f414a375ae1f99a34556252f41cdc18dd
Robin Gower <robin.git@infonomics.ltd.uk>
2021-03-09 13:41:34 +0100

    Configure Omni and Ansible to log to /var/log/ook

--- deploy/ansible/scripts/install.sh
+++ deploy/ansible/scripts/install.sh
@@ -12,13 +12,13 @@ echo '>>> OOK'
 
 echo '>>> making ook dirs'
 # for logs
-sudo mkdir -p /var/lib/elasticsearch/logs/ook
-sudo chown -R ook /var/lib/elasticsearch/logs/ook
+sudo mkdir -p /var/log/ook
+sudo chown -R ook /var/log/ook
 
 # TODO: move into ook package?
 sudo ln -s /etc/systemd/system/ook.service /etc/systemd/system/multi-user.target.wants/ook.service
 
-echo '>>> starting mut service'
+echo '>>> starting ook service'
 sudo systemctl start ook
 
 wget -nv -O - --retry-connrefused --tries 30 --waitretry=2 localhost:3010 2>&1 > /dev/null | uniq
