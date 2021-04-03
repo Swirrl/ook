b2fc40d3400d81d52ce25d02012e258b4757cc4f
Kira McLean <email@kiramclean.com>
2021-04-02 23:12:47 -0400

    store code checked state in app db

--- src/ook/handler.clj
+++ src/ook/handler.clj
@@ -12,8 +12,7 @@
   (fn [_request]
     (let [facets-with-codelists (->> (db/get-facets db)
                                      (map (fn [facet]
-                                            (assoc facet ;; probably stop doing this and get the codes
-                                                   ;; for a given facet when selected instead
+                                            (assoc facet
                                                    :codelists
                                                    (db/components->codelists
                                                     db
@@ -62,62 +61,29 @@
       invalid-format-response)))
 
 (def cl-data
-  [{:label "Standard Industrial Trade Commodities v4" :disabled? true :allow-any? true
-    :children [{:label "Animal & vegetable oils, fats & waxes" :disabled? true
-                :children [{:label "Animal oils and fats and their fractions, nes, whether or not refined, but not chemically modified"}
-                           {:label "Fats of bovine animals, sheep or goats, raw or rendered, whether or not pressed or solvent-extracted"}
-                           {:label "Wool grease, crude" :disabled? true}]}
-               {:label "Beverages & tobacco" :disabled? true
-                :children [{:label "Beverages" :disabled? true
-                            :children [{:label "Beer made from malt (including ale, stout and porter)"}
-                                       {:label "Fermented beverages, nes (eg cider, perry, mead)"}]}]}
-               {:label "Chemicals & related products, nes" :disabled true
+  [{:ook/uri "/def/code1" :label "Standard Industrial Trade Commodities v4" :disabled? true :allow-any? true
+    :children [{:label "Animal & vegetable oils, fats & waxes" :disabled? true :ook/uri "/def/code2"
+                :children [{:ook/uri "/def/code3"
+                            :label "Animal oils and fats and their fractions, nes, whether or not refined, but not chemically modified"}
+                           {:ook/uri "/def/code4"
+                            :label "Fats of bovine animals, sheep or goats, raw or rendered, whether or not pressed or solvent-extracted"}
+                           {:ook/uri "/def/code5"
+                            :label "Wool grease, crude" :disabled? true}]}
+               {:ook/uri "/def/code6" :label "Beverages & tobacco" :disabled? true
+                :children [{:ook/uri "/def/code7" :label "Beverages" :disabled? true
+                            :children [{:ook/uri "/def/code8" :label "Beer made from malt (including ale, stout and porter)"}
+                                       {:ook/uri "/def/code9" :label "Fermented beverages, nes (eg cider, perry, mead)"}]}]}
+              {:ook/uri "/def/code10" :label "Chemicals & related products, nes" :disabled true
                 :children [] ;; to provide disclosed example
                 }]}
-   {:label "Product" :disabled? true :allow-any? true
-    :children [{:label "Total" :disabled? true
-                :children [{:label "A Products of agriculture, forestry & fishing" :disabled? true
-                            :children [{:label "01 Products of agriculture"}
-                                       {:label "02 Forestry products"}]}]}]}])
+   {:ook/uri "/def/code11" :label "Product" :disabled? true :allow-any? true
+    :children [{:ook/uri "/def/code12" :label "Total" :disabled? true
+                :children [{:ook/uri "/def/code13" :label "A Products of agriculture, forestry & fishing" :disabled? true
+                            :children [{:ook/uri "/def/code14" :label "01 Products of agriculture"}
+                                       {:ook/uri "/def/code15" :label "02 Forestry products"}]}]}]}])
 
 (defmethod ig/init-key :ook.handler/codes [_ {:keys [search/db]}]
   (fn [request]
     (if (requesting-transit? request)
       (transit-response cl-data) ;; get real data for the give codelists/top level codes here
       invalid-format-response)))
-
-;; (defn- icon [& elements]
-;;   [:svg.me-1 {:xmlns "http://www.w3.org/2000/svg" :width 16 :height 16 :fill "black" :viewBox "0 0 16 16"}
-;;    elements])
-
-;; (def icon-down
-;;   (icon
-;;    [:path {:d "M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"}]))
-
-;; (def icon-up
-;;   (icon
-;;    [:path {:d "m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"}]))
-
-;; (declare list-group-item)
-
-;; (defn- list-group [elements]
-;;   [:ul.list-group-flush
-;;    (map list-group-item elements)])
-
-;; (defn- list-group-item [{:keys [label children disabled? allow-any?] :as element}]
-;;   (let [id (keyword label)]
-;;     [:li.list-group-item
-;;      (when children
-;;        (if (empty? children) ;; this would actually be controlled by disclosure interaction
-;;          icon-up
-;;          icon-down))
-;;      [:input.form-check-input.me-1 (merge {:type "checkbox" :id id} (when disabled? {:disabled true}))]
-;;      [:label.form-check-label {:for id} label]
-;;      (when children
-;;        [:a.ms-1.link-primary (if allow-any? "any" "all children")])
-;;      (when children
-;;        (list-group children))]))
-
-;; (defn- codelist-example []
-;;   [:div
-;;    (list-group cl-data)])
--- src/ook/reframe/views/filters.cljs
+++ src/ook/reframe/views/filters.cljs
@@ -6,35 +6,35 @@
    [ook.util :as u]))
 
 #_(defn filters []
-  (let [codes @(rf/subscribe [:results.codes/data])
-        query @(rf/subscribe [:results.codes/query])
-        cnt (count codes)]
-    (when codes
-      (list
-       ^{:key "answer"}
-       [:p "Found " [:strong cnt (u/pluralize " code" cnt)] " matching " [:strong "\"" query "\"."]]
-       (when (seq codes)
-         [:p "Select all the codes that you're interested in."])
-       ^{:key "data"}
-       [:form {:id "codes" :on-submit apply-code-selection}
-        (doall
-         (for [{code-id :code/id dim-id :dim/id label :code/label} codes
-               :let [value (str dim-id "," code-id)]]
-           ^{:key code-id} [:div.form-check.mb-3.bg-light
-                       [:div.p-2
-                        [:input.form-check-input
-                         {:type "checkbox"
-                          :name "code"
-                          :value value
-                          :checked (-> @(rf/subscribe [:ui.codes/selection]) (get value))
-                          :id code-id
-                          :on-change #(rf/dispatch [:ui.codes/toggle-selection
-                                                    (-> % .-target .-value)])}]
-                        [:label.form-check-label {:for code-id}
-                         [:strong (single-label label)]
-                         [:p.m-0 "id: " [:code code-id]]]]]))
-        (when (seq codes)
-          [:button.btn.btn-primary.mt-2.mb-4 {:type "submit"} "Find datasets that use these codes"])]))))
+    (let [codes @(rf/subscribe [:results.codes/data])
+          query @(rf/subscribe [:results.codes/query])
+          cnt (count codes)]
+      (when codes
+        (list
+         ^{:key "answer"}
+         [:p "Found " [:strong cnt (u/pluralize " code" cnt)] " matching " [:strong "\"" query "\"."]]
+         (when (seq codes)
+           [:p "Select all the codes that you're interested in."])
+         ^{:key "data"}
+         [:form {:id "codes" :on-submit apply-code-selection}
+          (doall
+           (for [{code-id :code/id dim-id :dim/id label :code/label} codes
+                 :let [value (str dim-id "," code-id)]]
+             ^{:key code-id} [:div.form-check.mb-3.bg-light
+                              [:div.p-2
+                               [:input.form-check-input
+                                {:type "checkbox"
+                                 :name "code"
+                                 :value value
+                                 :checked (-> @(rf/subscribe [:ui.codes/selection]) (get value))
+                                 :id code-id
+                                 :on-change #(rf/dispatch [:ui.codes/toggle-selection
+                                                           (-> % .-target .-value)])}]
+                               [:label.form-check-label {:for code-id}
+                                [:strong (single-label label)]
+                                [:p.m-0 "id: " [:code code-id]]]]]))
+          (when (seq codes)
+            [:button.btn.btn-primary.mt-2.mb-4 {:type "submit"} "Find datasets that use these codes"])]))))
 
 (defn- apply-filter-button [{:keys [codelists selection]}]
   [:button.btn.btn-primary.mt-3
@@ -45,17 +45,22 @@
 
 (declare code-list)
 
-(defn- code-list-item [{:keys [label children disabled? allow-any?]}]
+(defn- code-list-item [{:keys [ook/uri label children disabled? allow-any?]}]
   ^{:key label}
-  [:li.list-group-item.border-0
+  [:li.list-group-item.border-0.pb-0
    (when children
      (if (empty? children)
        icons/up
        icons/down))
-   [:input.form-check-input.mx-2 (cond-> {:type "checkbox" :id label}
-                                   disabled? (merge {:disabled true}))]
+   [:input.form-check-input.mx-2
+    (cond-> {:type "checkbox"
+             :name "code"
+             :value uri
+             :id uri
+             :checked (-> @(rf/subscribe [:ui.facets.current/codelist-selected? uri]))
+             :on-change #(rf/dispatch [:ui.facets.current/toggle-selection (-> % .-target .-value)])}
+      disabled? (merge {:disabled true}))]
    [:label.form-check-label.d-inline {:for label} label]
-   (js/console.log "CHILDREN::: " children)
    (when children
      [:<>
       [:a.ms-1.link-primary (if allow-any? "any" "all children")]
@@ -102,7 +107,7 @@
      [apply-filter-button selected-facet]
      (if  (seq (:codelists selected-facet))
        [code-selection selected-facet]
-         [no-codelist-message selected-facet])]))
+       [no-codelist-message selected-facet])]))
 
 (defn- facet-button [{:keys [name] :as facet} selected-facet]
   ^{:key name}
