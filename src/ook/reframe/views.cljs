9eec02680134c8097c9a06a23de58d30b9acc76f
Kira McLean <email@kiramclean.com>
2021-04-22 21:43:32 -0400

    wrap ui in react error boundary

--- src/ook/reframe/views.cljs
+++ src/ook/reframe/views.cljs
@@ -1,11 +1,12 @@
 (ns ook.reframe.views
   (:require
    [re-frame.core :as rf]
+   [ook.reframe.error-boundary :as err]
    [ook.reframe.facets.view :as facets]
    [ook.reframe.datasets.view :as datasets]))
 
 (defn search []
-  [:<>
+  [err/error-boundary
    [facets/configured-facets]
    [datasets/results]])
 
