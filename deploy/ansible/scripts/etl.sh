c4b30c2623f35e5e733a1842c02ab8e29ff08e78
Robin Gower <robin.git@infonomics.ltd.uk>
2021-05-14 14:15:25 +0200

    Introduce etl service unit

--- deploy/ansible/scripts/etl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sudo systemd-run --unit ook-indexer bash -c 'cd /opt/ook && java -Dlog4j.configurationFile=etl-log4j2.xml -cp "ook.jar:lib/*" -Xmx3g clojure.main -m ook.index'
