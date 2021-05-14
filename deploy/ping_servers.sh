835ddee979b4a034aefdaf58f7a15359b4db9133
Robin Gower <robin.git@infonomics.ltd.uk>
2021-05-14 13:45:41 +0200

    Distinguish production and staging inventory
    NB: the filename needs to end with "gcp.yml". Not sure why!
    
    https://docs.ansible.com/ansible/2.9/plugins/inventory/gcp_compute.html

--- deploy/ping_servers.sh
+++ deploy/ping_servers.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-echo "Pinging GCP servers..."
+echo
+echo "Pinging production servers..."
 
-env GCP_SERVICE_ACCOUNT_FILE=$GCLOUD_ACCOUNT_FILE ansible "*" -i ansible/gcp.yml -m ansible.builtin.ping
+env GCP_SERVICE_ACCOUNT_FILE=$GCLOUD_ACCOUNT_FILE ansible "*" -i ansible/production-gcp.yml -m ansible.builtin.ping
