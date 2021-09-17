d274b671b56a7b960c4654f969c270bab10f9b0c
Kira McLean <email@kiramclean.com>
2021-09-17 11:19:21 -0300

    update links to pmd to use new query param encoding

--- src/ook/params/util.cljs
+++ src/ook/params/util.cljs
@@ -1,24 +1,31 @@
 (ns ook.params.util
   (:require [reitit.impl :as ri]
-            [cemerick.url :as url]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [swirrl.qb-filters-dsl :as dsl]))
 
 (def base-uri "http://gss-data.org.uk/")
 (def pmd-uri "https://beta.gss-data.org.uk/")
 
-(defn- pair-dimensions-and-codes [{dim-uri :ook/uri codes :codes}]
-  (when codes
-    (map (fn [{code-uri :ook/uri}] [dim-uri code-uri]) codes)))
+(defn- to-sets-of-codes [result {dim-uri :ook/uri codes :codes}]
+  (if codes
+    (let [code-uris (map :ook/uri codes)]
+      (assoc result dim-uri (set code-uris)))
+    result))
 
 (defn absolute-uri [uri]
   (if (str/starts-with? uri "http")
     uri
     (str base-uri uri)))
 
-(defn- encode-pmd-style [[dim val]]
-  (str (url/url-encode (absolute-uri dim))
-       ","
-       (url/url-encode (absolute-uri val))))
+(defn- absolutize-uris [[dim vals]]
+  [(absolute-uri dim) (->> vals (map absolute-uri) set)])
+
+(defn- encode-pmd-style [data]
+  (->> data
+       (map (fn [[dim vals]]
+              [dim
+               (mapv (fn [val] [:select [:individual val]]) vals)]))
+       dsl/serialize))
 
 (defn- remove-matches-where-codelist-applied
   "If the user specifies a codelist then the matching codes are just examples not criteria.
@@ -26,7 +33,7 @@
   [applied-facets dataset-facets]
   (let [applied-codelist? (->> applied-facets
                                (mapcat val)
-                               (filter (fn [[codelist codes]] (empty? codes)))
+                               (filter (fn [[_codelist codes]] (empty? codes)))
                                (map key)
                                set)]
     (map (fn [facet] (update facet :dimensions (partial map (fn [dimension] (update dimension :codes (partial remove (fn [code] (some applied-codelist? (map :ook/uri (:scheme code)))))))))) dataset-facets)))
@@ -38,13 +45,14 @@
   (->> dataset-facets
        (remove-matches-where-codelist-applied applied-facets)
        (mapcat :dimensions)
-       (mapcat pair-dimensions-and-codes)
-       (remove nil?)
-       (map encode-pmd-style)))
+       (reduce to-sets-of-codes {})
+       (map absolutize-uris)
+       (into {})
+       encode-pmd-style))
 
 (defn link-to-pmd-dataset [id dataset-facets applied-facets]
   (let [filter-facets (encode-filter-facets dataset-facets applied-facets)
         query-string (str (ri/query-string (cond-> {:uri (str base-uri id)}
                                              (seq filter-facets) (assoc :apply-filters true
-                                                                        :filter-facets filter-facets))))]
+                                                                        :qb-filters filter-facets))))]
     (str pmd-uri "cube/explore?" query-string)))
