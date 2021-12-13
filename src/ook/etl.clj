cc2acaea96b6e28cea69e870d4867e283bbc69b2
Robin Gower <robin.git@infonomics.ltd.uk>
2021-12-13 15:37:46 +0100

    Revert "Attempt smaller page size for codes-used pipeline"
    This reverts commit 8c8e44512cbc25e53183ba0b429cff883f8014e1.
    
    Although only a config change, the commit we're reverting would've required the
    fixtures cassette to be refreshed. The new data is quite different and doesn't
    look quite right. Rather than work through this now I'm reverting the commit.
    
    The reverted commit was only intended to reduce the page size on the codes-used
    pipeline. Now that write-activity on the beta site has slowed down it seems the etl
    run is succeeding with the old config anyway!

--- src/ook/etl.clj
+++ src/ook/etl.clj
@@ -296,7 +296,7 @@
   (dataset-pipeline system)
   (component-pipeline system)
   (code-pipeline system)
-  (let [system (assoc system :ook.etl/select-page-size 100)]
+  (let [system (assoc system :ook.etl/select-page-size 200)]
     (code-used-pipeline system))
   (observation-pipeline system)
   (log/info "All pipelines complete"))
